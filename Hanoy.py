64c9b1857adc1f14e74c0555ab103014ae96c1bf
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2020-03-11 13:44:42 +0700

    Update Error

--- Hanoy.py
+++ Hanoy.py
@@ -1,6 +1,6 @@
 def hanoi(disks, source, auxiliary, target):
     if disks == 1:
-        print('Move disk 1 from peg {} to peg {}.'.format(source, target))
+        print('Move disk 1 from location {} to location {}.'.format(source, target))
         return
 
     hanoi(disks - 1, source, target, auxiliary)
